affa0a7b8fbe65772d9ecff46a9dc7d5542704ff
msamarpanraj <>
2023-12-10 10:46:39 +0000

    Add questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,199 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How many days and nights did it rain during the flood?",
+        answer: "B) 40 days and 40 nights",
+        options: [
+            "A) 30 days and 30 nights",
+            "B) 40 days and 40 nights",
+            "C) 50 days and 50 nights",
+            "D) 60 days and 60 nights",
+        ],
+        explanation:
+            "The Bible states that it rained for 40 days and 40 nights during the great flood (Genesis 7:12).",
+    },
+    {
+        numb: 2,
+        question:
+            "What kind of bird did Noah first send out from the ark to check for dry land?",
+        answer: "B) Raven",
+        options: ["A) Dove", "B) Raven", "C) Sparrow", "D) Eagle"],
+        explanation:
+            "Noah sent out a raven first, which did not return to the ark, indicating that it had found dry land (Genesis 8:7).",
+    },
+    {
+        numb: 3,
+        question: "How many passengers were on the ark?",
+        answer: "A) 8",
+        options: ["A) 8", "B) 10", "C) 12", "D) 20"],
+        explanation:
+            "There were eight people on the ark: Noah, his wife, his three sons (Shem, Ham, and Japheth), and their wives (Genesis 7:13).",
+    },
+    {
+        numb: 4,
+        question: "What did Noah use to seal the ark?",
+        answer: "D) Pitch",
+        options: ["A) Tar", "B) Cement", "C) Wax", "D) Pitch"],
+        explanation:
+            "Noah sealed the ark using pitch, a waterproofing material (Genesis 6:14).",
+    },
+    {
+        numb: 5,
+        question: "How many of each clean animal did Noah take on the ark?",
+        answer: "C) 7",
+        options: ["A) 2", "B) 5", "C) 7", "D) 10"],
+        explanation:
+            "Seven of each clean animal were taken on the ark (Genesis 7:2-3).",
+    },
+    {
+        numb: 6,
+        question: "How many stories or levels did the ark have?",
+        answer: "B) 3",
+        options: ["A) 1", "B) 3", "C) 2", "D) 4"],
+        explanation: "The ark had three stories or levels (Genesis 6:16).",
+    },
+    {
+        numb: 7,
+        question:
+            "What sign did God give as a promise to never flood the earth again?",
+        answer: "A) A rainbow",
+        options: [
+            "A) A rainbow",
+            "B) A comet",
+            "C) A solar eclipse",
+            "D) A thunderstorm",
+        ],
+        explanation:
+            "God's covenant sign was a rainbow, a symbol of His promise to never flood the earth again (Genesis 9:12-17).",
+    },
+    {
+        numb: 8,
+        question: "Who built the ark?",
+        answer: "C) Noah",
+        options: ["A) Abraham", "B) Moses", "C) Noah", "D) David"],
+        explanation:
+            "Noah was the righteous man chosen by God to build the ark and save his family and animals from the flood (Genesis 6:9-22).",
+    },
+    {
+        numb: 9,
+        question: "How long did the flood waters prevail on the earth?",
+        answer: "B) 40 days",
+        options: ["A) 20 days", "B) 40 days", "C) 60 days", "D) 100 days"],
+        explanation:
+            "The flood waters prevailed on the earth for 40 days and nights (Genesis 7:12).",
+    },
+    {
+        numb: 10,
+        question:
+            "What did the dove bring back to Noah, signaling that there was dry land?",
+        answer: "B) A twig from a fig tree",
+        options: [
+            "A) A leaf from an olive tree",
+            "B) A twig from a fig tree",
+            "C) A grain of wheat",
+            "D) A pebble",
+        ],
+        explanation:
+            "The dove brought back a freshly plucked olive leaf, indicating that there was dry land (Genesis 8:11).",
+    },
+    {
+        numb: 11,
+        question: "What was the purpose of the ark during the flood?",
+        answer: "B) To provide shelter for Noah and his family",
+        options: [
+            "A) To house all the animals in the world",
+            "B) To provide shelter for Noah and his family",
+            "C) To transport people to a new land",
+            "D) To serve as a lighthouse for lost ships",
+        ],
+        explanation:
+            "The primary purpose of the ark was to provide shelter for Noah, his family, and the selected animals during the flood.",
+    },
+    {
+        numb: 12,
+        question:
+            "Which mountain did the ark come to rest on after the flood waters receded?",
+        answer: "C) Mount Ararat",
+        options: [
+            "A) Mount Sinai",
+            "B) Mount Nebo",
+            "C) Mount Ararat",
+            "D) Mount Carmel",
+        ],
+        explanation:
+            "The ark came to rest on Mount Ararat after the flood (Genesis 8:4).",
+    },
+    {
+        numb: 13,
+        question: "What covenant did God establish with Noah after the flood?",
+        answer: "C) The Covenant of the Rainbow",
+        options: [
+            "A) The Covenant of Circumcision",
+            "B) The Covenant of the Sabbath",
+            "C) The Covenant of the Rainbow",
+            "D) The Covenant of Sacrifices",
+        ],
+        explanation:
+            "God established the Covenant of the Rainbow, promising never to flood the earth again (Genesis 9:12-17).",
+    },
+    {
+        numb: 14,
+        question: "How many pairs of clean animals did Noah take on the ark?",
+        answer: "D) 7 pairs",
+        options: ["A) 1 pair", "B) 2 pairs", "C) 3 pairs", "D) 7 pairs"],
+        explanation:
+            "Noah took seven pairs of each clean animal on the ark, which would be used for sacrifices (Genesis 7:2-3).",
+    },
+    {
+        numb: 15,
+        question:
+            "Who was the first person mentioned in the Bible as getting drunk after the flood?",
+        answer: "B) Ham",
+        options: ["A) Noah", "B) Ham", "C) Shem", "D) Japheth"],
+        explanation:
+            "Ham, one of Noah's sons, was the first person mentioned as getting drunk after the flood (Genesis 9:21).",
+    },
+    {
+        numb: 16,
+        question:
+            "What did God instruct Noah and his family to be fruitful and multiply?",
+        answer: "D) Humans",
+        options: ["A) Fish", "B) Birds", "C) Animals", "D) Humans"],
+        explanation:
+            "God instructed Noah and his family to be fruitful and multiply, ensuring the continuation of humanity (Genesis 9:1).",
+    },
+    {
+        numb: 17,
+        question:
+            "How many days did Noah send out each bird before receiving a sign of dry land?",
+        answer: "C) 7 days",
+        options: ["A) 1 day", "B) 3 days", "C) 7 days", "D) 14 days"],
+        explanation:
+            "Noah sent out each bird for seven days before receiving a sign of dry land (Genesis 8:10-12).",
+    },
+    {
+        numb: 18,
+        question: "What was the name of Noah's wife?",
+        answer: "D) Naamah",
+        options: ["A) Mary", "B) Sarah", "C) Leah", "D) Naamah"],
+        explanation: "According to tradition, Noah's wife's name is Naamah.",
+    },
+    {
+        numb: 19,
+        question:
+            "Who was the last person to leave the ark after the flood waters receded?",
+        answer: "A) Noah",
+        options: ["A) Noah", "B) Shem", "C) Ham", "D) Japheth"],
+        explanation:
+            "Noah was the last person to leave the ark, followed by his sons (Genesis 8:18-19).",
+    },
+    {
+        numb: 20,
+        question:
+            "What did Noah build as a symbol of God's covenant after the flood?",
+        answer: "A) An altar",
+        options: ["A) An altar", "B) A tower", "C) A temple", "D) A wall"],
+        explanation:
+            "After the flood, Noah built an altar as a symbol of God's covenant (Genesis 8:20).",
+    },
+];
